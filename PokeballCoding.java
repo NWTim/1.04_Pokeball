8359460daf09998b55eb34d4c478ca0cc5f539dc
NWTim <43040718+NWTim@users.noreply.github.com>
2018-09-11 08:30:05 -0700

    Add files via upload

---
+++ PokeballCoding.java
@@ -0,0 +1,18 @@
+
+public class PokeballCoding {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("   @@@@@@@@@@");
+System.out.println("  @@@@@@@@@@@@");
+System.out.println(" @@@@@@@@@@@@@@");
+System.out.println("@@@@@@@@@@@@@@@@");
+System.out.println("|||||[\"\"\"\"]|||||");
+System.out.println("|||||[....]|||||");
+System.out.println("oooooooooooooooo");
+System.out.println(" oooooooooooooo");
+System.out.println("  oooooooooooo");
+System.out.println("   oooooooooo");
+	}
+
+}
